ddc05ddbc0205bf051377b2f799a11d911859458
Amirul Nazheef <91238564+amirulnazheef@users.noreply.github.com>
2026-03-24 15:53:11 +0900

    Default
    Default change

--- user/init.c
+++ user/init.c
@@ -25,10 +25,6 @@ main(void)
 
   for(;;){
     printf("init: starting sh\n");
-    printf("Student ID: 2023313239\n");
-    printf("Name: Amirul Nazheef\n");
-    printf("========== Your Message ==========\n");
-    printf("         Welcome to xv6 OS        \n");
     pid = fork();
     if(pid < 0){
       printf("init: fork failed\n");
